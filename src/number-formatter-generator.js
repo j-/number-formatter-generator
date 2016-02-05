cd193b91f0741756a17bbb7e699e974398220004
j- <j@skeoh.com>
2016-02-05 15:53:02 +1100

    Add some basic line comments

--- src/number-formatter-generator.js
+++ src/number-formatter-generator.js
@@ -1,10 +1,13 @@
 var EXP_MASK = /^.*?([0-9+\-.,' #]+).*?$/;
 
 module.exports = function (input) {
+	// Do not format values if not given a mask
 	if (!input) {
 		return function format (input) { return input };
 	}
+	// Ensure input is string
 	input = String(input);
+	// Extract actual mask from input
 	var match = input.match(EXP_MASK);
 	if (!match) {
 		throw new Error('Invalid mask');
@@ -13,6 +16,7 @@ module.exports = function (input) {
 	var prefix = input.substring(0, match.index);
 	var suffix = input.substring(match.index + mask.length);
 	return function format (input) {
+		// Pass value through if not numeric
 		if (isNaN(input)) {
 			return input;
 		}
