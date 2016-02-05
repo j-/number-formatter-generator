173ea9180cf804b1b951e6130ab438a4a557720d
j- <j@skeoh.com>
2016-02-05 15:48:42 +1100

    Detect mask pattern in input

--- src/number-formatter-generator.js
+++ src/number-formatter-generator.js
@@ -1,4 +1,14 @@
-module.exports = function (mask) {
+var EXP_MASK = /^.*?([0-9+\-.,' #]+).*?$/;
+
+module.exports = function (input) {
+	input = String(input);
+	var match = input.match(EXP_MASK);
+	if (!match) {
+		throw new Error('Invalid mask');
+	}
+	var mask = match[1];
+	var prefix = input.substring(0, match.index);
+	var suffix = input.substring(match.index + mask.length);
 	return function format (input) {
 
 	};
